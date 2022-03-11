af890ef0ab3d4f1e68ddd55a0d6c29b0cbfbe140
mitchsteamy <91216056+mitchsteamy@users.noreply.github.com>
2022-03-10 22:56:16 -0500

    Update Pong_Mitch_Embry.py

--- Pong_Mitch_Embry.py
+++ Pong_Mitch_Embry.py
@@ -1,4 +1,6 @@
 #import pygame
+from operator import mul
+from pickle import FALSE, TRUE
 import pygame, sys
 
 
@@ -51,7 +53,7 @@ class Ball:
         self.y_pos += self.y_vel
 
     def bounce(self):
-        if (self.x_pos + self.x_vel <= p1_paddle.x_pos + p1_paddle.width) and (p1_paddle.y_pos < self.y_pos + self.y_vel + self.rad < p1_paddle.y_pos + p1_paddle.height):
+        if (self.x_pos + self.x_vel < p1_paddle.x_pos + p1_paddle.width) and (p1_paddle.y_pos < self.y_pos + self.y_vel + self.rad < p1_paddle.y_pos + p1_paddle.height + self.rad):
             self.x_vel = -self.x_vel
             self.y_vel = (p1_paddle.y_pos + p1_paddle.height / 2 - self.y_pos )/15 #test
             self.y_vel = -self.y_vel
@@ -61,7 +63,7 @@ class Ball:
             self.y_pos = HEIGHT / 2
             self.x_vel = 5
             self.y_vel = 0
-        if (self.x_pos + self.x_vel >= p2_paddle.x_pos - p2_paddle.width) and (p2_paddle.y_pos < self.y_pos + self.y_vel + self.rad < p2_paddle.y_pos + p2_paddle.height):
+        if (self.x_pos + self.x_vel > p2_paddle.x_pos - p2_paddle.width) and (p2_paddle.y_pos < self.y_pos + self.y_vel + self.rad < p2_paddle.y_pos + p2_paddle.height +self.rad):
             self.x_vel = -self.x_vel
             self.y_vel = (p2_paddle.y_pos + p2_paddle.height / 2 - self.y_pos )/ 15 #test
             self.y_vel = -self.y_vel
@@ -76,7 +78,7 @@ class Ball:
 
 
 class Paddle:
-    def __init__(self, screen, color, x_pos, y_pos, width, height) -> None:
+    def __init__(self, screen, color, x_pos, y_pos, width, height):
         self.screen = screen
         self.color = color
         self.x_pos = x_pos
@@ -102,8 +104,44 @@ class Paddle:
             self.y_pos = HEIGHT - self.height
 
 
+class AI_Paddle:
+    def __init__(self, screen, color, x_pos, y_pos, width, height):
+        self.screen = screen
+        self.color = color
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.width = width
+        self.height = height
+        self.score = 0
+        self.speed = - 5
+        self.difficulty = "medium"
+
+
+    def set_difficulty(self):
+        if self.difficulty == "easy":
+            self.speed += 2
+        if self.difficulty == "hard":
+            self.speed -= 2 
+
+
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, (self.x_pos, self.y_pos, self.width, self.height))
+
+    def move(self):
+        if self.y_pos > ball.y_pos and ball.x_vel > 0 and ball.x_pos > WIDTH//2:
+            self.y_pos += self.speed
+        elif self.y_pos < ball.y_pos and ball.x_vel > 0 and ball.x_pos > WIDTH//2:
+            self.y_pos += -self.speed
+        
+        if self.y_pos <= 0:
+            self.y_pos = 0
+        if self.y_pos >= HEIGHT - self.height:
+            self.y_pos = HEIGHT - self.height
+
+
 class Score_Board:  
-    def __init__(self) -> None:
+    def __init__(self):
         self.game_font = pygame.font.SysFont('Ubuntu', 50)      
     
     def draw(self):
@@ -118,19 +156,26 @@ class Score_Board:
         
 
 class Titles:
-    def __init__(self) -> None:
+    def __init__(self):
         self.screen = screen
         self.game_font = pygame.font.SysFont('Ubuntu', 40)
+        self.game_font_med = pygame.font.SysFont('Ubuntu', 30)
         self.game_font_small = pygame.font.SysFont('Ubuntu', 20)
-
 
         self.intro()
 
     def intro(self):
         screen.fill(BLACK)
-        self.intro = self.game_font.render(F"Press the space bar to begin playing.", False, WHITE)
-        screen.blit(self.intro,(WIDTH // 9 , HEIGHT // 2 - 40))
-    
+        self.intro = self.game_font_med.render(F"Press M for Multiplayer Mode. Press Space to begin playing.", False, WHITE)
+        screen.blit(self.intro,(WIDTH // 23 , HEIGHT // 2 - 40))
+
+    def difficulty(self):
+        screen.fill(BLACK)
+        self.difficulty = self.game_font_med.render(F"Press 1 for Easy, 2 for Medium, or 3 for Hard.", False, WHITE)
+        screen.blit(self.difficulty,(WIDTH // 7, HEIGHT // 2 - 40))
+
+
+
     def win(self):
         global playing
         self.p1_win = self.game_font.render(F"Player 1 is the winner.", False, BLUE)
@@ -156,24 +201,38 @@ class Titles:
 #object instances
 ball = Ball(screen, WHITE, WIDTH // 2, HEIGHT // 2, LINE_WEIGHT * 2) 
 p1_paddle = Paddle(screen, BLUE, 10, (HEIGHT // 2 - 10 * LINE_WEIGHT), 2 * LINE_WEIGHT, 20 * LINE_WEIGHT)
-p2_paddle = Paddle(screen, RED, WIDTH - 20, (HEIGHT // 2 - 10 * LINE_WEIGHT), 2 * LINE_WEIGHT, 20 * LINE_WEIGHT)
+p2_paddle = AI_Paddle(screen, RED, WIDTH - 20, (HEIGHT // 2 - 10 * LINE_WEIGHT), 2 * LINE_WEIGHT, 20 * LINE_WEIGHT)
 score = Score_Board()
 titles = Titles()
 
 #main function
 running = True
 playing = False
+multi_player_mode = False
 
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit()   
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
+            if event.key == pygame.K_m:
+                p2_paddle = Paddle(screen, RED, WIDTH - 20, (HEIGHT // 2 - 10 * LINE_WEIGHT), 2 * LINE_WEIGHT, 20 * LINE_WEIGHT)
+                playing = True
             if event.key == pygame.K_SPACE:
-                playing = True    
+                titles.difficulty()
+            if event.key == pygame.K_1:
+               p2_paddle.difficulty = "easy"
+               p2_paddle.set_difficulty()
+               playing = True
+            if event.key == pygame.K_2:
+                playing = True
+            if event.key == pygame.K_3:
+                p2_paddle.difficulty = "hard"
+                p2_paddle.set_difficulty
+                playing = True
             if event.key == pygame.K_w:
                 p1_paddle.state = "up"
             if event.key == pygame.K_s:
@@ -191,13 +250,13 @@ while running:
 
         p1_paddle.move()
         p1_paddle.draw()
-
-        p2_paddle.move()
-        p2_paddle.draw()
         
         ball.move()
         ball.draw()
         ball.bounce()
+
+        p2_paddle.move()
+        p2_paddle.draw()
         
         score.draw()
         titles.win()
